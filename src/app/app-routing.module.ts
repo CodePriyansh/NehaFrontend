01f9526fe8af2c2243a420290dcf6daff2dc7026
developer_aman <developer_aman@example.com>
2022-04-14 15:22:41 +0530

    neha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdmincustomerComponent } from './admincustomer/admincustomer.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { EditcategoryComponent } from './editcategory/editcategory.component';
+import { OrderComponent } from './order/order.component';
+import { ViewcategoryComponent } from './viewcategory/viewcategory.component';
+import { ViewproductComponent } from './viewproduct/viewproduct.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AdmindashboardComponent},
+  {path:'editcategory',component:EditcategoryComponent},
+  {path:'viewcategory',component:ViewcategoryComponent},
+  {path:'viewproduct',component:ViewproductComponent},
+  {path:'order',component:OrderComponent},
+  {path:'customer',component:AdmincustomerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategorySectionComponent } from './category-section/category-section.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { EditcategoryComponent } from './editcategory/editcategory.component';
+import { ViewcategoryComponent } from './viewcategory/viewcategory.component';
+import { ViewproductComponent } from './viewproduct/viewproduct.component';
+import { OrderComponent } from './order/order.component';
+import { AdmincustomerComponent } from './admincustomer/admincustomer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent,
+    CategorySectionComponent,
+    AdmindashboardComponent,
+    EditcategoryComponent,
+    ViewcategoryComponent,
+    ViewproductComponent,
+    OrderComponent,
+    AdmincustomerComponent
   ],
   imports: [
     BrowserModule,
